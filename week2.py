5119046db27e3104fc0e57861b019f8a7fa80348
Sushil Lohani <CSMSLOHA@ljmu.ac.uk>
2024-01-23 11:26:47 +0000

    lecture week 2

--- week2.py
+++ week2.py
@@ -1,7 +1,15 @@
 numbers = ["one","two","three","four"]
 #prnt the whole list
 print(numbers)
+
 #computing the length
 print(len(numbers))
+
 #print the first element of a list 
 print(numbers[0])
+
+#print in uppercase
+print(numbers[0].upper())
+
+#access the last element
+print(numbers[-1])
