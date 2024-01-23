4fcc1963f4640e6ddeabfa548aef79356c0d1575
Sushil Lohani <CSMSLOHA@ljmu.ac.uk>
2024-01-23 11:34:22 +0000

    week 2

--- week2.py
+++ week2.py
@@ -13,3 +13,27 @@ print(numbers[0].upper())
 
 #access the last element
 print(numbers[-1])
+
+#modifying asn element 
+numbers[0]= "zero"
+print(numbers)
+
+#adding an element at the end
+numbers.append("ten")
+print(numbers)
+
+#inserting an element 
+numbers.insert(2,"ten")
+print(numbers)
+
+#deleting an element
+del numbers[1]
+print(numbers)
+
+#removing an element by name 
+numbers.remove("ten")
+print(numbers)
+
+#reusing deleted item
+x=numbers.pop(2)
+print(numbers)
